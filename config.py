2255cd373ee9f28e0ba35d2d449ec1f7e7a3c83c
Kryvonis <kryvonis.artem@gmail.com>
2016-08-25 10:23:08 +0300

    add gitignore and configuration

--- config.py
+++ config.py
@@ -1,2 +1,44 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__name__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_THEARDOWN = True
+    FLASKY_MAIL_SUBJECT = '[Flasky]'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASS')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    #                           'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestConfig(Config):
+    TESTING = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    #                           'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #                           'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    pass
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig,
+}
